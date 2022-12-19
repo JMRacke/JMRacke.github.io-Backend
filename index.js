478769525dfae270cf8fe28ccd6e008ec6f53c88
Joe Racke <joeracke@gmail.com>
2022-12-19 14:19:15 -0800

    try again

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// enable cors
+app.use(cors());
+
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000,
@@ -20,8 +23,5 @@ app.use("/api", require("./routes/yelp"));
 
 // Set static folder
 app.use(express.static("public"));
-
-// enable cors
-app.use(cors());
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
